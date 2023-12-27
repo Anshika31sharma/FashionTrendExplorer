d47cc30a5d96d49b154724b32ec86af0c689cc93
Anshika31sharma <sharma.anshika7251@gmail.com>
2023-12-27 22:14:50 +0530

    added components

---
+++ src/App.js
@@ -0,0 +1,202 @@
+import React, { useState, useEffect } from 'react';
+import Suggestion from './Suggestion';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+import './App.css';
+
+const MyntraClothesPage = () => {
+  const [isHovered, setIsHovered] = useState(false);
+  const [showProducts, setShowProducts] = useState(false);
+  const [brandsFilter, setBrandsFilter] = useState([]);
+  const [ratingFilter, setRatingFilter] = useState([]);
+  const [wishlist, setWishlist] = useState([]);
+  const [products, setProducts] = useState([]);
+
+  const handleHover = () => {
+    setIsHovered(true);
+  };
+
+  const handleLeave = () => {
+    setIsHovered(false);
+  };
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      setShowProducts(true);
+      e.preventDefault(); 
+    }
+  };
+
+  const fetchProductsData = async () => {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products/category/women's%20clothing");
+      const data = await response.json();
+      setProducts(data);
+    } catch (error) {
+      console.error('Error fetching products:', error);
+    }
+  };
+
+  useEffect(() => {
+    fetchProductsData();
+  }, []);
+
+  const isPriceInRange = (price, range) => {
+    const [lower, upper] = range.split(' - ').map((str) => parseFloat(str.replace(/[^\d.]/g, '')));
+    const productPrice = parseFloat(price.replace(/[^\d.]/g, ''));
+    return productPrice >= lower && productPrice <= upper;
+  };
+
+  const applyFilters = () => {
+    let filteredProducts = [...products];
+
+    if (brandsFilter.length > 0) {
+      filteredProducts = filteredProducts.filter((product) => brandsFilter.includes(product.brand));
+    }
+
+    if (ratingFilter.length > 0) {
+      filteredProducts = filteredProducts.filter((product) => ratingFilter.includes(product.rating));
+    }
+
+    return filteredProducts;
+  };
+
+  const handleBrandFilterChange = (brand) => {
+    setBrandsFilter((prevBrands) => {
+      if (prevBrands.includes(brand)) {
+        return prevBrands.filter((b) => b !== brand);
+      } else {
+        return [...prevBrands, brand];
+      }
+    });
+  };
+
+  const handleRatingFilterChange = (rating) => {
+    setRatingFilter((prevRatings) => {
+      if (prevRatings.includes(rating)) {
+        return prevRatings.filter((r) => r !== rating);
+      } else {
+        return [...prevRatings, rating];
+      }
+    });
+  };
+
+  const clearAllFilters = () => {
+    setBrandsFilter([]);
+    setRatingFilter([]);
+  };
+
+  const toggleWishlist = (productId) => {
+    setWishlist((prevWishlist) => {
+      if (prevWishlist.includes(productId)) {
+        return prevWishlist.filter((id) => id !== productId);
+      } else {
+        return [...prevWishlist, productId];
+      }
+    });
+  };
+
+
+  return (
+    <div className="container mt-3">
+      <div className="d-flex justify-content-center align-items-center">
+        <div
+          className={`search-placeholder bg-white p-3 border border-1 rounded cursor-pointer position-relative ${isHovered ? 'hovered' : ''}`}
+          contentEditable
+          onKeyDown={handleKeyDown}
+          onMouseEnter={handleHover}
+          onMouseLeave={handleLeave}
+          tabIndex={0}
+        >
+          Search
+          {isHovered && !showProducts && <Suggestion />}
+        </div>
+      </div>
+      {showProducts && (
+        <div className="row mt-3">
+         <div className="col-md-3">
+      <div className="card">
+        <div className="card-body">
+          <h5 className="card-title">Filters</h5>
+          <div>
+            <h6 className="card-subtitle mb-2 text-muted">Brands</h6>
+            {['Brand A', 'Brand B', 'Brand C'].map((brand) => (
+              <div key={brand} className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id={`brand-${brand}`}
+                  checked={brandsFilter.includes(brand)}
+                  onChange={() => handleBrandFilterChange(brand)}
+                />
+                <label className="form-check-label" htmlFor={`brand-${brand}`}>
+                  {brand}
+                </label>
+              </div>
+            ))}
+          </div>
+          <div>
+            <h6 className="card-subtitle mb-2 text-muted">Ratings</h6>
+            {[1, 2, 3, 4, 5].map((rating) => (
+              <div key={rating} className="form-check">
+                <input
+                  className="form-check-input"
+                  type="checkbox"
+                  id={`rating-${rating}`}
+                  checked={ratingFilter.includes(rating)}
+                  onChange={() => handleRatingFilterChange(rating)}
+                />
+                <label className="form-check-label" htmlFor={`rating-${rating}`}>
+                  {Array.from({ length: rating }, (_, index) => (
+                    <span key={index} className="bi bi-star-fill text-warning mr-1"></span>
+                  ))}
+                </label>
+              </div>
+            ))}
+          </div>
+          <button className="btn btn-primary mt-3" onClick={clearAllFilters}>
+            Clear All
+          </button>
+        </div>
+      </div>
+    </div>
+    <div className="col-md-9">
+<div className="row">
+  {applyFilters().map((product) => (
+    <div
+      key={product.id}
+      className="col-sm-6 col-md-4 col-lg-3 product-card" 
+      onMouseEnter={() => setIsHovered(product.id)}
+      onMouseLeave={() => setIsHovered(null)}
+    >
+      <div className="card mb-3 w-100">
+        <img src={product.image} alt={product.name} className="card-img-top product-image img-fluid" />
+        <div className="card-body justify-content-between d-flex">
+          <div>
+            <p className="card-text">{product.brand}</p>
+            <p className="card-text">{product.price}</p>
+          
+          </div>
+          <div>
+            <FontAwesomeIcon
+              icon={faHeart}
+              className={`mr-2${wishlist.includes(product.id) ? ' text-danger' : ' text-muted'}`}
+              onClick={() => toggleWishlist(product.id)}
+            />
+            {isHovered === product.id && (
+              <button className="btn btn-primary mt-2">View Product</button>
+            )}
+          </div>
+        </div>
+      </div>
+    </div>
+  ))}
+</div>
+    </div>
+  </div>
+)}
+    </div>
+  );
+};
+export default MyntraClothesPage;
---
+++ src/Suggestion.js
@@ -0,0 +1,47 @@
+import React from 'react'
+export default function suggestion() {
+  return (
+<div className="suggestion-box position-absolute start-0 border border-1 border-secondary bg-white shadow p-3 z-index-1 w-100">
+    <p className="p-1">Latest Trends</p>
+   <div className="d-flex justify-content-between">
+  
+     <div className="suggestion-card">
+       <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ8OfqlX6-VC5yKQ-_eaq6CIH24KaK4OFl2iA&usqp=CAU" />
+       <span>Product 1</span>
+     </div>
+     <div className="suggestion-card">
+       <img src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/15508746/2021/10/8/76d93c26-46fa-4946-826c-8957e2ff173c1633676949601-SASSAFRAS-Women-Jumpsuit-9261633676949313-1.jpg" alt="Product 2" />
+       <span>Product 2</span>
+     </div>
+     <div className="suggestion-card">
+       <img src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/24251476/2023/7/29/2fecdcb2-6eb0-47df-aec7-8109992610f21690621383817ClothingSet1.jpg" alt="Product 3" />
+       <span>Product 3</span>
+     </div>
+     <div className="suggestion-card">
+       <img src="https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/12414994/2020/10/5/3ed22679-bb13-4132-81bf-1ee0574ffa331601888051173-Sangria-Women-Dresses-5081601888049484-1.jpg" alt="Product 4" />
+       <span>Product 4</span>
+     </div>
+     <div className="suggestion-card">
+       <img src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/24283556/2023/8/1/bd9214e3-b2ee-4c5c-afe6-ef4ba6f7a8c51690894330669BAESDWomenBlackRelaxedStraightLegTrousers1.jpg" alt="Product 3" />
+       <span>Product 5</span>
+     </div>
+     <div className="suggestion-card">
+       <img src="https://assets.myntassets.com/dpr_1.5,q_60,w_400,c_limit,fl_progressive/assets/images/18076128/2022/5/16/7c80524f-56e6-4425-a129-c43ddd039f9e1652677781758-Saree-mall-Black-Crepe-Ethnic-Digital-Print-Festive-Wear-Sar-1.jpg" alt="Product 3" />
+       <span>Product 6</span>
+     </div>
+     <div className="suggestion-card ">
+       <img src="https://assets.myntassets.com/h_1440,q_100,w_1080/v1/assets/images/15598130/2021/10/11/00b95453-af40-4cfe-a8fb-51960607777e1633926686080-DILLINGER-Women-Tshirts-7421633926685543-1.jpg" alt="Product 3" />
+       <span>Product 7</span>
+     </div>
+   </div>
+   <div className="p-1">
+     <span>Popular Suggestions:</span>
+     <div className=" d-flex align-items-center p-2 border-bottom">Suggestion 1</div>
+     <div className=" d-flex align-items-center p-2 border-bottom">Suggestion 2</div>
+     <div className=" d-flex align-items-center p-2 border-bottom">Suggestion 3</div>
+     <div className=" d-flex align-items-center p-2 border-bottom">Suggestion 4</div>
+     <div className=" d-flex align-items-center p-2 border-bottom">Suggestion 5</div>
+   </div>
+ </div>
+  )
+}
